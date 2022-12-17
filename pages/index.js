43099a57311a950ebb6222002633f077b627d653
devMuangi <devmuangi@gmail.com>
2022-12-17 10:11:14 +0000

    init

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Index({ posts, globalData }) {
   <Link href="/appsmith/appsmith">gutheria</Link>
 </h1>
         <About aboutGutheria={globalData.aboutGutheria}/>
-        <AppsmithEmbed/>
+        {/* <AppsmithEmbed/> */}
         <Mission missionGutheria={globalData.missionGutheria}/>
         <ul className="w-full">
           {posts.map((post) => (
