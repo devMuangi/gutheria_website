1ab51588ae7851b1af683dd37b7958bdc5e82ac9
devMuangi <devmuangi@gmail.com>
2022-12-17 08:35:53 +0000

    init

--- utils/global-data.js
+++ utils/global-data.js
@@ -4,16 +4,16 @@ export const getGlobalData = () => {
     : 'gutheria';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'For a better environment.';
+    : 'For a better ,greener environment.';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
   const aboutGutheria = process.env.BLOG_ABOUT
     ? decodeURI(process.env.BLOG_ABOUT)
-    :'We are dedicated to making a better environment for future generations by reusing recycling and reducing waste materials in our immediate environment.';
+    :'We are dedicated to making a better environment for future generations by reusing recycling and reducing waste materials in our immediate environment.We are dedicated to making a better environment for future generations by reusing recycling and reducing waste materials in our immediate environment.We are dedicated to making a better environment for future generations by reusing recycling and reducing waste materials in our immediate environment.We are dedicated to making a better environment for future generations by reusing recycling and reducing waste materials in our immediate environment.';
   const missionGutheria = process.env.BLOG_MISSION
     ? decodeURI(process.env.BLOG_MISSION)
-    : 'Our mission is to provide a clean environment for prosperity';
+    : 'Our mission is to provide a clean environment for prosperity.Our mission is to provide a clean environment for prosperity.';
 
   return {
     name,
