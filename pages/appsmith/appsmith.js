3af1233b27fd8f8410bfd001f9e256f217a7557b
devMuangi <devmuangi@gmail.com>
2022-12-17 10:51:22 +0000

    init

--- pages/appsmith/appsmith.js
+++ pages/appsmith/appsmith.js
@@ -2,7 +2,7 @@ export default function Appsmith() {
     return (
       <div className="App">
         <h3>Gutheria portal:</h3>
-        <iframe src="https://app.appsmith.com/app/gutheria/homepage-639870fc038c74195c0ab098?branch=main?embed=true" height="700" width="100%"></iframe>
+        <iframe src="https://app.appsmith.com/app/gutheria/homepage-639870fc038c74195c0ab098?branch=main" height="700" width="100%"></iframe>
       </div>
     );
   }
