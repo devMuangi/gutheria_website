a0bccf45bc4be8cc2e4b539a2664e8fd23e58c4b
devMuangi <devmuangi@gmail.com>
2022-12-20 06:00:31 +0000

    init

--- components/Navbar.js
+++ components/Navbar.js
@@ -8,31 +8,30 @@ export const Navbar = ({name}) => {
   return (
     <>
       <nav className='flex justify-center flex-wrap'>
-        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
         <div>
           <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto'>
-            <Link href='/'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
-                Services
-              </a>
-            </Link>
-            <Link href='/'>
+          <Link href='/about/about'>
               <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
                 About us
               </a>
             </Link>
-            <Link href='/'>
+            <Link href='/services/services'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+                Services
+              </a>
+            </Link>
+            <Link href='/contact/contact'>
               <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
                 Contact us
               </a>
             </Link>
-            <Link href='/'>
+            <Link href='/knowledge/knowledge'>
               <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
                 Knowledge Base
               </a>
             </Link>
             <Link href='/appsmith/appsmith'>
-              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white'>
+              <a className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-black'>
                  <Link href="/appsmith/appsmith">Gutheria portal</Link>
               </a>
             </Link>
---
+++ pages/knowledge/knowledge.js
@@ -0,0 +1,5 @@
+export default function Knowledge() {
+    return <h1>Read our blog posts here in the :Knowledge page</h1>;
+  }
+
+  
