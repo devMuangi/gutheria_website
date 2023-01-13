0aa227548d06ad33fc80a4be783737beca280770
devMuangi <devmuangi@gmail.com>
2023-01-13 12:38:23 +0000

    init

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,11 +1,9 @@
 import { getGlobalData  } from "../../utils/global-data";
 import SEO from "../../components/SEO";
-import Header from "../../components/Header";
 
 export default function About({ globalData }) {
     return (<>
       <SEO title={globalData.name} description={globalData.blogTitle} /> 
-      <Header name={globalData.name} /> 
       <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1> 
